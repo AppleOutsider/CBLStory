f21373997eb677fa00a03e74b0bbc0d0bda5ed9f
whaeundo <1916388@sookmyung.ac.kr>
2023-03-27 15:23:38 +0900

    [Setting] Font style setting

--- MC1_CBL.swiftpm/Extension/FontExtension.swift
+++ MC1_CBL.swiftpm/Extension/FontExtension.swift
@@ -8,12 +8,11 @@
 import SwiftUI
 
 extension Font {
-    static let customMedium32 = Font.custom("GmarketSansMedium", size: 32)
-    static let customMedium36 = Font.custom("GmarketSansMedium", size: 36)
-    static let customMedium40 = Font.custom("GmarketSansMedium", size: 40)
-    static let customMedium44 = Font.custom("GmarketSansMedium", size: 44)
+    static let customMedium27 = Font.custom("GmarketSansMedium", size: 27)
+    static let customMedium30 = Font.custom("GmarketSansMedium", size: 30)
+    static let customMedium33 = Font.custom("GmarketSansMedium", size: 33)
     
-    static let customLight44 = Font.custom("GmarketSansLight", size: 44)
+    static let customLight30 = Font.custom("GmarketSansLight", size: 30)
     
     
 }
