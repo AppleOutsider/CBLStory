b91e90630f6f1e241da99748dc5d9ecd0afc849e
dayjack <frostjanna12@gmail.com>
2023-03-28 22:21:39 +0900

    [Feat/#15] Update ViewExtension, lineTextView

--- MC1_CBL.swiftpm/View/CustomView/lineTextView.swift
+++ MC1_CBL.swiftpm/View/CustomView/lineTextView.swift
@@ -1,93 +1,93 @@
 //
 //  SwiftUIView.swift
-//  
+//
 //
 //  Created by YU WONGEUN on 2023/03/25.
 //
 
 import SwiftUI
 
-struct LineTextView: View {
-    // MARK: - variable
-    @State var textCnt: Int = 0
-    @State var text: String = " "
-    @State var name: String = " "
-    @State var lineModels: LineModels   // 데이터
-    @State var touchValid: Bool = true  // 대사 typewriter() 중 터치 불가
-    @State var hiddenText: Bool = false // 대사 박스 숨길지 말지
-    @State var personPosition: LineModels.personPosition = .none    // 이 값에 따라 대사 박스 바뀜
-    
-    // geometryproxy 값 받아서 가변적으로 사이즈 조정
-    // position에 따라 모서리 깎이게 바꿔주기
-    // MARK: - body
-    var body: some View {
-        
-        HStack(alignment: .center) {
-            
-            Button {
-                //                    typeWriter()
-                touchValid = false
-            } label: {
-//                switch personPosition {
-//                case .left:
-//                    CustomWhiteLeft()
-//                case .right:
-//                    CustomWhiteRight()
-//                case .center:
-//                    CustomWhiteCenter()
-//                case .none: // 내레이션
-//                    CustomBlackCenter()
-//                }
-                
-            } // label
-            .frame(width: 1472, height: 279, alignment: .leading)
-            .customHidden(hiddenText)
-            // 대사 중에 터치 불가
-            .allowsHitTesting(touchValid)
-            // 배경 위치 잡을 때
-            .offset(y: 370)
-        } // HStack
-    } // body
-    
-    // MARK: - Function
-    
-    //    func typeWriter(at position: Int = 0) {
-    //        if position == 0 {
-    //            text = ""
-    //        }
-    //        if position < lineModels.lines[textCnt].count {
-    //            // write the letter at position "position" to "text"
-    //            DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
-    //                print(position)
-    //                print(textCnt)
-    //                name = lineModels.names[textCnt]
-    //                text.append(lineModels.lines[textCnt][position])
-    //                // call this this function again with character at the next position
-    //                typeWriter(at: position + 1)
-    //            }
-    //        } else {
-    //            touchValid = true
-    //            // 재귀함수 종료 시 textCnt를 증가시켜 다음 인덱스로 넘어가게 했습니다.
-    //            textCnt += 1
-    //        }
-    //    }
-    //
-    // 대사 박스 전체 숨길 때 호출
-    func textHidden(){
-        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-            hiddenText = true
-        }
-    }
-    // 대사 박스 전체 보여줄 때 호출
-    func textExposed() {
-        hiddenText = false
-    }
-}
+//struct LineTextView: View {
+//    // MARK: - variable
+//    @State var textCnt: Int = 0
+//    @State var text: String = " "
+//    @State var name: String = " "
+//    @State var lineModels: LineModels   // 데이터
+//    @State var touchValid: Bool = true  // 대사 typewriter() 중 터치 불가
+//    @State var hiddenText: Bool = false // 대사 박스 숨길지 말지
+//    @State var personPosition: LineModels.personPosition = .none    // 이 값에 따라 대사 박스 바뀜
+//
+//    // geometryproxy 값 받아서 가변적으로 사이즈 조정
+//    // position에 따라 모서리 깎이게 바꿔주기
+//    // MARK: - body
+//    var body: some View {
+//
+//        HStack(alignment: .center) {
+//
+//            Button {
+//                //                    typeWriter()
+//                touchValid = false
+//            } label: {
+////                switch personPosition {
+////                case .left:
+////                    CustomWhiteLeft()
+////                case .right:
+////                    CustomWhiteRight()
+////                case .center:
+////                    CustomWhiteCenter()
+////                case .none: // 내레이션
+////                    CustomBlackCenter()
+////                }
+//
+//            } // label
+//            .frame(width: 1472, height: 279, alignment: .leading)
+//            .customHidden(hiddenText)
+//            // 대사 중에 터치 불가
+//            .allowsHitTesting(touchValid)
+//            // 배경 위치 잡을 때
+//            .offset(y: 370)
+//        } // HStack
+//    } // body
+//
+//    // MARK: - Function
+//
+//    //    func typeWriter(at position: Int = 0) {
+//    //        if position == 0 {
+//    //            text = ""
+//    //        }
+//    //        if position < lineModels.lines[textCnt].count {
+//    //            // write the letter at position "position" to "text"
+//    //            DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+//    //                print(position)
+//    //                print(textCnt)
+//    //                name = lineModels.names[textCnt]
+//    //                text.append(lineModels.lines[textCnt][position])
+//    //                // call this this function again with character at the next position
+//    //                typeWriter(at: position + 1)
+//    //            }
+//    //        } else {
+//    //            touchValid = true
+//    //            // 재귀함수 종료 시 textCnt를 증가시켜 다음 인덱스로 넘어가게 했습니다.
+//    //            textCnt += 1
+//    //        }
+//    //    }
+//    //
+//    // 대사 박스 전체 숨길 때 호출
+//    func textHidden(){
+//        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+//            hiddenText = true
+//        }
+//    }
+//    // 대사 박스 전체 보여줄 때 호출
+//    func textExposed() {
+//        hiddenText = false
+//    }
+//}
 
 // MARK: - 뷰 를 어떻게 해보려고 ...
 
 // 베이스로 사용하자
-func CustomTextView(lineModels: LineModels) -> some View {
+func CustomTextView(lineModels: LineModels, geo: GeometryProxy) -> some View {
     HStack {
         VStack(alignment: .leading, spacing: 30) {
             
@@ -95,7 +95,8 @@ func CustomTextView(lineModels: LineModels) -> some View {
                 .bold()
                 .textPersonBlack()
             
-            GradientLine(startPoint: .leading, endPoint: .trailing, colors: [.gray, .white])
+            GradientLine(startPoint: .leading, endPoint: .trailing, colors: [.black.opacity(1), .black.opacity(0)])
+                .gradientFrame(geo: geo)
             
             Text(lineModels.lines)
                 .bold()
@@ -111,15 +112,17 @@ func CustomTextView(lineModels: LineModels) -> some View {
 }
 
 // 내레이션 베이스
-func CustomtextBlack(lineModels: LineModels) -> some View {
+func CustomtextBlack(lineModels: LineModels, geo: GeometryProxy) -> some View {
     HStack {
         VStack(alignment: .leading, spacing: 30) {
             
             Text(lineModels.names)
                 .bold()
-                .textPersonWhite() // .textPersonWhite(geo: GeoProxy)  geo.size.width - 40 geo.size.height / 4 
+                .textPersonWhite()
+                // .textPersonWhite(geo: GeoProxy)  geo.size.width - 40 geo.size.height / 4
             
-            GradientLine(startPoint: .leading, endPoint: .trailing, colors: [.white, .gray])
+            GradientLine(startPoint: .leading, endPoint: .trailing, colors: [.white.opacity(1), .white.opacity(0)])
+                .gradientFrame(geo: geo)
             
             Text(lineModels.lines)
                 .bold()
@@ -136,44 +139,27 @@ func CustomtextBlack(lineModels: LineModels) -> some View {
 
 // MARK: - 여기서 4가지 버전으로
 // 흰 배경 왼쪽 대사
-func CustomWhiteLeft(name: String, line: String, soundEffect: String?) -> some View {
-    CustomTextView(lineModels: LineModels(names: name, lines: line, soundEffect: nil))
-        .textWhiteLeft()
+func CustomWhiteLeft(name: String, line: String, soundEffect: String?, geo: GeometryProxy) -> some View {
+    CustomTextView(lineModels: LineModels(names: name, lines: line, soundEffect: nil), geo: geo)
+        .textWhiteLeft(geo: geo)
 }
 
 // 흰 배경 오른쪽 대사
-func CustomWhiteRight(name: String, line: String, soundEffect: String?) -> some View {
-    CustomTextView(lineModels: LineModels(names: name, lines: line, soundEffect: nil))
-        .textWhiteRight()
+func CustomWhiteRight(name: String, line: String, soundEffect: String?, geo: GeometryProxy) -> some View {
+    CustomTextView(lineModels: LineModels(names: name, lines: line, soundEffect: nil), geo: geo)
+        .textWhiteRight(geo: geo)
 }
 
 // 흰 배경 가운데 대사
-func CustomWhiteCenter(name: String, line: String, soundEffect: String?) -> some View {
-    CustomTextView(lineModels: LineModels(names: name, lines: line, soundEffect: nil))
-        .textWhiteCenter()
+func CustomWhiteCenter(name: String, line: String, soundEffect: String?, geo: GeometryProxy) -> some View {
+    CustomTextView(lineModels: LineModels(names: name, lines: line, soundEffect: nil), geo: geo)
+        .textWhiteCenter(geo: geo)
 }
 
 // 검은 배경 내래이션
-func CustomBlackCenter(name: String, line: String, soundEffect: String?) -> some View {
-    CustomTextView(lineModels: LineModels(names: name, lines: line, soundEffect: nil))
-        .textBlack()
+func CustomBlackCenter(name: String, line: String, soundEffect: String?, geo: GeometryProxy) -> some View {
+    CustomtextBlack(lineModels: LineModels(names: name, lines: line, soundEffect: nil), geo: geo)
+        .textBlack(geo: geo)
 }
 
 
-
-
-// MARK: - Previews
-struct lineTextView_Previews: PreviewProvider {
-    static var previews: some View {
-        
-        VStack {
-            //            lineTextView(lineModels: DataModels(), personPosition: .none)
-            //            lineTextView(lineModels: DataModels(), personPosition: .left)
-            //            lineTextView(lineModels: DataModels(), personPosition: .right)
-            //            lineTextView(lineModels: DataModels(), personPosition: .center)
-            //            Spacer()
-            CustomWhiteLeft(name: "testname", line: "testline", soundEffect: nil)
-        }
-        .background(Image("bg"))
-    }
-}
