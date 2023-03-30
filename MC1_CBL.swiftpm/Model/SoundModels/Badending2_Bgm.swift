f68601eedad00055be1c066b422777acec220415
whaeundo <1916388@sookmyung.ac.kr>
2023-03-30 15:22:57 +0900

    [Feat/#57] Add Bgm Models

---
+++ MC1_CBL.swiftpm/Model/SoundModels/Badending2_Bgm.swift
@@ -0,0 +1,37 @@
+//
+//  File.swift
+//
+//
+//  Created by ChoiYujin on 2023/03/29.
+//
+
+import Foundation
+import AVKit
+
+class Badending2_Bgm: ObservableObject {
+    
+    static let instance = Badending2_Bgm()
+    var player: AVAudioPlayer?
+    
+    func playSound() {
+        
+        guard let url = Bundle.main.url(forResource: "mc1_bgm_badending2", withExtension: ".mp3") else { return }
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.play()
+        } catch let error {
+            print("재생하는데 오류가 발생했습니다. \(error.localizedDescription)")
+        }
+    }
+    func stopSound() {
+        guard let url = Bundle.main.url(forResource: "mc1_bgm_badending2", withExtension: ".mp3") else { return }
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.stop()
+        } catch let error {
+            print("재생하는데 오류가 발생했습니다. \(error.localizedDescription)")
+        }
+    }
+}
+
+
