b7db0300a1372bf9075ed0faa97c108790f4392b
dayjack <frostjanna12@gmail.com>
2023-03-30 21:06:36 +0900

    [Setting/#64] Add resized images & Set bgm loop twice

--- MC1_CBL.swiftpm/Model/SoundModels/Chapter4_1Bgm.swift
+++ MC1_CBL.swiftpm/Model/SoundModels/Chapter4_1Bgm.swift
@@ -18,6 +18,7 @@ class Chapter4_1Bgm: ObservableObject {
         guard let url = Bundle.main.url(forResource: "mc1_bgm_school", withExtension: ".mp3") else { return }
         do {
             player = try AVAudioPlayer(contentsOf: url)
+            player?.numberOfLoops = 2
             player?.play()
         } catch let error {
             print("재생하는데 오류가 발생했습니다. \(error.localizedDescription)")
