5bc3dc72905c32ae95ab49bd90c3bc20c2159f99
Ryu <ywg6525@daum.net>
2023-03-30 19:01:26 +0900

    [Setting] Set App Icon

--- MC1_CBL.swiftpm/Package.swift
+++ MC1_CBL.swiftpm/Package.swift
@@ -20,7 +20,7 @@ let package = Package(
             teamIdentifier: "UG6UT639QH",
             displayVersion: "1.0",
             bundleVersion: "1",
-            appIcon: .placeholder(icon: .cloud),
+            appIcon: .asset("AppIcon"),
             accentColor: .presetColor(.brown),
             supportedDeviceFamilies: [
                 .pad,
